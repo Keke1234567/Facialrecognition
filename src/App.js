ca8c27154f4d72f334d7b2f244e1695be3e210f4
Keke (mac) <jere.ljungberg@gmail.com>
2018-06-03 19:52:00 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,97 @@
+import React, { Component } from 'react';
+import './App.css';
+import Navigation from './Components/Navigation/Navigation';
+import Logo from './Components/Logo/Logo';
+import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
+import Rank from './Components/Rank/Rank';
+import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+import FaceRecognition from './Components/FaceRecognition/FaceRecognition';
+import SignIn from './Components/SignIn/SignIn';
+
+const app = new Clarifai.App({
+ apiKey: 'ddb57512f2df44e3a24fdb9b27020057'
+});
+
+const particlesOptions = {
+    particles: {
+        number: {
+            value: 30,
+              density: {
+                enable: true,
+                value_area: 800
+              }
+          }
+      }
+  }
+
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: {},
+      route: 'signin'
+    }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiface = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiface.left_col * width,
+      topRow: clarifaiface.top_row * height,
+      rightCol: width - (clarifaiface.right_col * width),
+      bottomRow: height - (clarifaiface.bottom_row * height)  
+    }
+  }
+
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({box: box});
+  }
+
+  onInputChange = (event) => {
+    this.setState( { input: event.target.value })
+  }
+
+  onSubmit = () => {
+    this.setState( { imageUrl: this.state.input })
+    app.models.predict(
+      Clarifai.FACE_DETECT_MODEL,
+      this.state.input)
+    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+    .catch(err => console.log(err)); 
+  }
+
+  onRouteChange = (param) => {
+    this.setState({ route: param });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Particles 
+              className='particles'
+              params={{ particlesOptions }}
+            />
+        <Navigation onRouteChange={this.onRouteChange} />
+      { this.state.route === 'signin'
+        ? <SignIn onRouteChange={this.onRouteChange} />
+        : <div>
+            <Logo />
+            <Rank />
+            <ImageLinkForm onInputChange = {this.onInputChange} onButtonSubmit = {this.onSubmit} />
+            <FaceRecognition box = {this.state.box} imageUrl = {this.state.imageUrl} />
+          </div>
+      }
+      </div>
+    );
+  }
+}
+
+export default App;
